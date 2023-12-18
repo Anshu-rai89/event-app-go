feb9a95d1d67deada8aa5216a063610c5880a4d2
Anshu Rai <raianshu8991@gmail.com>
2023-12-18 22:25:17 +0530

    Added getAllEvents

--- models/event.go
+++ models/event.go
@@ -39,6 +39,26 @@ func (e Event) Save() error {
 	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+
+	rows, err := db.DB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.Id, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
 }
